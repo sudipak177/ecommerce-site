06f498f327bd49c85ef02e99fb11fbccc19d4d48
sudipak177 <sudiprc177@gmail.com>
2025-09-21 09:32:58 +0545

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Cart from './pages/Cart'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Product from './pages/Product'
-import Login from './pages/Login'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <>
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element = {<Contact/>}/>
-        <Route path='/cart' element ={<Cart/>}/>
-        <Route path='/orders' element = {<Orders/>}/>
-        <Route path='/PlaceOrder' element = {<PlaceOrder/>}/>
-        <Route path='/Product/:productId' element = {<Product/>}/>
-        <Route path='/Login' element = {<Login/>}/>
-      </Routes>
-
-    </div>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { NavLink, Link } from "react-router-dom";
+
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <div className="flex items-center justify-between py-5 font-medium">
+        <NavLink to="/">
+          <img src={assets.logo} className="w-36" alt="this is logo" />
+        </NavLink>
+
+        <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+          <NavLink className="flex flex-col items-center gap-1" to="/">
+            <p>HOME</p>
+            <hr className=" w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink className="flex flex-col items-center gap-1" to="/about">
+            <p>ABOUT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink
+            className="flex flex-col items-center gap-1"
+            to="/collection"
+          >
+            <p>COLLECTION</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink className="flex flex-col items-center gap-1" to="/contact">
+            <p>CONTACT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </ul>
+        <div className="flex items-center gap-6">
+          <img
+            src={assets.search_icon}
+            alt="this is search icon"
+            className="w-5"
+          />
+          <div className="group relative">
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                <p className="cursor-pointer hover:text-black ">My Profile</p>
+                <p className="cursor-pointer hover:text-black ">Orders</p>
+                <p className="cursor-pointer hover:text-black ">Logout</p>
+              </div>
+            </div>
+          </div>
+          <Link to="/cart" className="relative">
+            <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px] ">
+              0
+            </p>
+          </Link>
+          <button className="sm:hidden" onClick={toggleNavbar}>
+            <img src={assets.menu_icon} className="w-5 cursor-pointer" alt="" />
+          </button>
+        </div>
+      </div>
+      {isOpen &&(
+      <div
+        className=" fixed sm:hidden flex inset-0 "
+        onClick={toggleNavbar}
+      > 
+
+        <ul className="flex flex-col items-center gap-5 left-0 h-full bg-amber-50 text-sm w-50 pt-6 shadow-lg text-gray-700 ">
+        <NavLink to="/">
+          <img src={assets.logo} className="w-36" alt="this is logo" />
+        </NavLink>
+          <NavLink className="flex flex-col items-center gap-1" to="/">
+            <p>HOME</p>
+            <hr className=" w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink className="flex flex-col items-center gap-1" to="/about">
+            <p>ABOUT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink
+            className="flex flex-col items-center gap-1"
+            to="/collection"
+          >
+            <p>COLLECTION</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+          <NavLink className="flex flex-col items-center gap-1" to="/contact">
+            <p>CONTACT</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        </ul>
+      </div>
+      )}
+    </>
+  );
+}
+
+export default NavBar;
